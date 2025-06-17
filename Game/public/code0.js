8a67d5fc3ac5da58f7bef7d8bd14227e5de9c086
Tyvation <daniel300068@gmail.com>
2025-06-17 11:27:53 +0800

    d

--- Game/public/code0.js
+++ Game/public/code0.js
@@ -1899,6 +1899,11 @@ if(isConditionTrue_1) {
 }
 }
 if (isConditionTrue_0) {
+isConditionTrue_0 = false;
+{isConditionTrue_0 = (runtimeScene.getGame().getVariables().getFromIndex(20).getAsNumber() < 99);
+}
+}
+if (isConditionTrue_0) {
 {runtimeScene.getGame().getVariables().getFromIndex(7).setBoolean(true);
 }{gdjs.evtTools.input.hideCursor(runtimeScene);
 }}
@@ -3100,7 +3105,7 @@ gdjs.StartSceneCode.eventsList43(runtimeScene);} //End of subevents
 
 };gdjs.StartSceneCode.eventsList45 = function(runtimeScene) {
 
-};gdjs.StartSceneCode.userFunc0x1a58500 = function GDJSInlineCode(runtimeScene) {
+};gdjs.StartSceneCode.userFunc0x8b07f0 = function GDJSInlineCode(runtimeScene) {
 "use strict";
 const difficulty = runtimeScene.getGame().getVariables().get("LevelDifficulty").getAsString();
 const leaderboard = runtimeScene.getGame().getVariables().get("LeaderBoardList").getChild(difficulty);
@@ -3146,7 +3151,7 @@ gdjs.StartSceneCode.eventsList46 = function(runtimeScene) {
 {
 
 
-gdjs.StartSceneCode.userFunc0x1a58500(runtimeScene);
+gdjs.StartSceneCode.userFunc0x8b07f0(runtimeScene);
 
 }
 
@@ -3448,7 +3453,7 @@ gdjs.copyArray(runtimeScene.getObjects("TileTomato"), gdjs.StartSceneCode.GDTile
 
 let isConditionTrue_0 = false;
 isConditionTrue_0 = false;
-{isConditionTrue_0 = (runtimeScene.getGame().getVariables().getFromIndex(20).getAsNumber() == 99);
+{isConditionTrue_0 = (runtimeScene.getGame().getVariables().getFromIndex(20).getAsNumber() == 1);
 }
 if (isConditionTrue_0) {
 
